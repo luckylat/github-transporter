3fba9e479deafda9079c853ea85a3303b81befad
luckylat <lacklat.key@gmail.com>
2023-05-04 18:20:41 +0900

    feat: add auto mode

--- main.go
+++ main.go
@@ -15,88 +15,108 @@ const filename = ".github-transporter"
 //TODO: Restore function
 
 func exportCommand() error {
-	//get origin URL
-	repositoryName, err := exec.Command("git", "remote", "get-url", "origin").Output()
-	if err != nil {
-		fmt.Println("cannot get git remote file")
-		return err
-	}
+    //get origin URL
+    repositoryName, err := exec.Command("git", "remote", "get-url", "origin").Output()
+    if err != nil {
+        fmt.Println("cannot get git remote file")
+        return err
+    }
 
-	//make `.github-transporter` file
-	dotfile, err := os.Create(filename)
-	if err != nil {
-		fmt.Println("cannot touch", filename, ".")
-		return err
-	}
-	defer dotfile.Close()
+    //make `.github-transporter` file
+    dotfile, err := os.Create(filename)
+    if err != nil {
+        fmt.Println("cannot touch", filename, ".")
+        return err
+    }
+    defer dotfile.Close()
 
-	dotfile.WriteString("remote = " + string(repositoryName))
+    dotfile.WriteString("remote = " + string(repositoryName))
 
-	if err := os.RemoveAll(".git"); err != nil {
-		fmt.Println("cannot remove .git file")
-		return err
-	}
-	return nil
+    if err := os.RemoveAll(".git"); err != nil {
+        fmt.Println("cannot remove .git file")
+        return err
+    }
+    return nil
 }
 
 func importCommand() error {
-	fileData, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println("cannot open", filename, ".")
-		return err
-	}
-	formattedFileData := strings.ReplaceAll(string(fileData), "\n", "")
+    fileData, err := ioutil.ReadFile(filename)
+    if err != nil {
+        fmt.Println("cannot open", filename, ".")
+        return err
+    }
+    formattedFileData := strings.ReplaceAll(string(fileData), "\n", "")
 
-	reg := regexp.MustCompile(`remote = `)
-	repositoryName := reg.ReplaceAllString(formattedFileData, "")
+    reg := regexp.MustCompile(`remote = `)
+    repositoryName := reg.ReplaceAllString(formattedFileData, "")
 
-	commandStr := "git init; git remote add origin " + repositoryName + "; git fetch;"
-	gitCommand := exec.Command("/bin/sh", "-c", commandStr)
+    commandStr := "git init; git remote add origin " + repositoryName + "; git fetch;"
+    gitCommand := exec.Command("/bin/sh", "-c", commandStr)
 
-	if err := gitCommand.Run(); err != nil {
-		fmt.Println("cannot add remote URL:", repositoryName, ".")
-		return err
-	}
+    if err := gitCommand.Run(); err != nil {
+        fmt.Println("cannot add remote URL:", repositoryName, ".")
+        return err
+    }
 
-	if err := os.Remove(".github-transporter"); err != nil {
-		fmt.Println("cannnot remote .github-transporter file")
-		return err
-	}
+    if err := os.Remove(".github-transporter"); err != nil {
+        fmt.Println("cannnot remote .github-transporter file")
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func existTransporterFile() bool {
-	_, err := os.Stat(filename)
-	return !os.IsNotExist(err)
+    _, err := os.Stat(filename)
+    return !os.IsNotExist(err)
 }
 
 func main() {
-	flag.Parse()
-	arg := flag.Args()
-	if len(arg) == 0 {
-		//Auto mode
-		if existTransporterFile() {
-			fmt.Println("Auto Mode: execute `Import` ok? [y/N]: ")
-		} else {
-			fmt.Println("Auto Mode: execute `Export` ok? [y/N]: ")
-		}
-	} else {
-		switch arg[0] {
-		case "export":
-			if err := exportCommand(); err != nil {
-				fmt.Println(err)
-				fmt.Println("Process Abort")
-				os.Exit(1)
-			}
-		case "import":
-			if err := importCommand(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		default:
-			fmt.Println("Invalid Command: `github-transporter {export/import}`")
-			return
-		}
-	}
+    flag.Parse()
+    arg := flag.Args()
+    var command string
+    if len(arg) == 0 {
+        //Auto mode
+        if existTransporterFile() {
+            fmt.Print("Auto Mode: execute `Import` ok? [y/N]: ")
+            var input string
+            fmt.Scan(&input)
+            if input == "y" {
+                command = "import"
+            }
+        } else {
+            fmt.Print("Auto Mode: execute `Export` ok? [y/N]: ")
+            var input string
+            fmt.Scan(&input)
+            if input == "y" {
+                command = "export"
+            }
+        }
+    } else {
+        switch arg[0] {
+        case "export":
+            command = "export"
+        case "import":
+            command = "import"
+        default:
+        }
+    }
+
+    switch command {
+    case "import":
+        if err := importCommand(); err != nil {
+            fmt.Println(err)
+            fmt.Println("Process Abort")
+            os.Exit(1)
+        }
+    case "export":
+        if err := exportCommand(); err != nil {
+            fmt.Println(err)
+            fmt.Println("Process Abort")
+            os.Exit(1)
+        }
+    default:
+        fmt.Println("Invalid Command: `github-transporter {export/import}`")
+        os.Exit(1)
+    }
 }

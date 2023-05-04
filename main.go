85bb127f54ed87aabb75dd144ed4022c5e61417a
luckylat <lacklat.key@gmail.com>
2023-05-04 18:03:55 +0900

    refactor: tab to space

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "os/exec"
     "regexp"
     "strings"
 )
@@ -45,23 +45,23 @@ func importCommand() error {
 		fmt.Println("cannot open", filename, ".")
 		return err
 	}
-    formattedFileData := strings.ReplaceAll(string(fileData), "\n", "")
+	formattedFileData := strings.ReplaceAll(string(fileData), "\n", "")
 
-    reg := regexp.MustCompile(`remote = `)
-    repositoryName := reg.ReplaceAllString(formattedFileData, "")
+	reg := regexp.MustCompile(`remote = `)
+	repositoryName := reg.ReplaceAllString(formattedFileData, "")
 
-    commandStr := "git init; git remote add origin " + repositoryName + "; git fetch;"
-    gitCommand := exec.Command("/bin/sh", "-c", commandStr)
+	commandStr := "git init; git remote add origin " + repositoryName + "; git fetch;"
+	gitCommand := exec.Command("/bin/sh", "-c", commandStr)
 
-    if err := gitCommand.Run(); err != nil {
-        fmt.Println("cannot add remote URL:", repositoryName, ".")
-        return err
-    }
+	if err := gitCommand.Run(); err != nil {
+		fmt.Println("cannot add remote URL:", repositoryName, ".")
+		return err
+	}
 
-    if err := os.Remove(".github-transporter"); err != nil {
-        fmt.Println("cannnot remote .github-transporter file")
-        return err
-    }
+	if err := os.Remove(".github-transporter"); err != nil {
+		fmt.Println("cannnot remote .github-transporter file")
+		return err
+	}
 
 	return nil
 }
@@ -84,16 +84,16 @@ func main() {
 	} else {
 		switch arg[0] {
 		case "export":
-            if err := exportCommand(); err != nil {
-                fmt.Println(err)
-                fmt.Println("Process Abort")
-                os.Exit(1)
-            }
+			if err := exportCommand(); err != nil {
+				fmt.Println(err)
+				fmt.Println("Process Abort")
+				os.Exit(1)
+			}
 		case "import":
-            if err := importCommand(); err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-            }
+			if err := importCommand(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		default:
 			fmt.Println("Invalid Command: `github-transporter {export/import}`")
 			return
